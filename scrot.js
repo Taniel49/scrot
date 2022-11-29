9fb9b82bd941194125d410153a2eb51493c052b9
Mikhail Butyrskiy <laughingstorm49@gmail.com>
2022-11-29 19:42:38 +0300

    import

--- scrot.js
+++ scrot.js
@@ -1,7 +1,7 @@
 const childProcess = require('child_process');
 
 module.exports = async function (name) {
-const scrot = childProcess.spawn("import", [-window, root, name+'.jpg']);
+const scrot = childProcess.spawn("import", ["-window", "root", name+'.jpg']);
 
 const exitCode = await new Promise( (resolve, reject) => {
 scrot.on('close', resolve);})
--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 module.exports = {
     getEncodedImage: function (name) {
-        const file = fs.readFileSync(`./${name}.png`);
+        const file = fs.readFileSync(`./${name}.jpg`);
         return Buffer.from(file).toString('base64');
     },
     writeRecognisedFile: function (name, text) {
