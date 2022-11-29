fb55869735f577f97b48e440682163bfd726b5ba
Mikhail Butyrskiy <laughingstorm49@gmail.com>
2022-11-29 18:51:31 +0300

    import

--- scrot.js
+++ scrot.js
@@ -1,7 +1,7 @@
 const childProcess = require('child_process');
 
 module.exports = async function (name) {
-const scrot = childProcess.spawn( "scrot", [name+'.png']);
+const scrot = childProcess.spawn("import", [-window, root, name+'.jpg']);
 
 const exitCode = await new Promise( (resolve, reject) => {
 scrot.on('close', resolve);})
